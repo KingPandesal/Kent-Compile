908ef24cd7cd8cceae1d03ae73e3f10d83f6c345
SixxCodes <z.madayag.141776.tc@umindanao.edu.ph>
2026-01-15 21:24:18 +0800

    Fix: .png to .jpeg. IT'S WORKING NA!!!!!! HAHAHAHAHAHHAHAA

--- src/faceMapper.ts
+++ src/faceMapper.ts
@@ -2,11 +2,11 @@ import * as vscode from 'vscode';
 import * as path from 'path';
 
 export function getFaceImage(errors: number): string {
-    if (errors === 0) {return '1.png';}
-    if (errors === 1) {return '2.png';}
-    if (errors === 2) {return '3.png';}
-    if (errors === 3) {return '4.png';}
-    if (errors === 4) {return '5.png';}
-    if (errors >= 5) {return '6.png';}
-    return '7.png';
+    if (errors === 0) {return '1.jpeg';}
+    if (errors === 1) {return '2.jpeg';}
+    if (errors === 2) {return '3.jpeg';}
+    if (errors === 3) {return '4.jpeg';}
+    if (errors === 4) {return '5.jpeg';}
+    if (errors >= 5) {return '6.jpeg';}
+    return '7.jpeg';
 }
