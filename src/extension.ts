b83f022f384e9e272da2e7baba5c8f5d6cab706b
SixxCodes <z.madayag.141776.tc@umindanao.edu.ph>
2026-01-15 16:31:45 +0800

    Fix: tasks.json deafult to compile to open Extension Host new window vscode.

--- src/extension.ts
+++ src/extension.ts
@@ -17,15 +17,15 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "kent-compile" is now active!');
+	console.log('Congratulations, your extension "kentCompile" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('kent-compile.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('kentCompile.onPanel', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from kent-compile!');
+		vscode.window.showInformationMessage('Hello Kalibutan from kent-compile!');
 	});
 
 	context.subscriptions.push(disposable);
--- src/faceMapper.ts
+++ src/faceMapper.ts
@@ -2,11 +2,11 @@ import * as vscode from 'vscode';
 import * as path from 'path';
 
 export function getFaceImage(errors: number): string {
-    if (errors === 0) return '1.png';
-    if (errors === 1) return '2.png';
-    if (errors === 2) return '3.png';
-    if (errors === 3) return '4.png';
-    if (errors === 4) return '5.png';
-    if (errors >= 5) return '6.png';
+    if (errors === 0) {return '1.png';}
+    if (errors === 1) {return '2.png';}
+    if (errors === 2) {return '3.png';}
+    if (errors === 3) {return '4.png';}
+    if (errors === 4) {return '5.png';}
+    if (errors >= 5) {return '6.png';}
     return '7.png';
 }
